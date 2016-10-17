76b3b0deb8c2ebee4295be9ada688d1281ec5795
Adidi <adieloz@gmail.com>
2016-10-17 16:58:25 +0300

    finish deploy script

--- .server/deploy/deploy.sh
+++ .server/deploy/deploy.sh
@@ -1,26 +1,39 @@
 #!/bin/sh
 
-BASE_PATH=/home/axonize/code
-PATH_WT=${BASE_PATH}/git/admin.$1.wt
-PATH_HTML=${BASE_PATH}/html
+BASE_PATH=/root/code/web/hila
+PATH_WT=${BASE_PATH}/wt
+PATH_GIT=${BASE_PATH/git
+PATH_WWW=${BASE_PATH}/www
+PATH_WWW_NEW=${BASE_PATH}/www.new
+PATH_WWW_OLD=${BASE_PATH}/www.old
+PM2_APP_NAME=hila
 
 set -x
-rm -fr ${PATH_HTML}/admin.$1.new
-cp -r ${PATH_WT} ${PATH_HTML}/admin.$1.new
-cp -r ${PATH_HTML}/admin.$1/node_modules ${PATH_HTML}/admin.$1.new
-cd ${PATH_HTML}/admin.$1.new
+
+#remove new dir if any
+rm -fr ${PATH_WWW_NEW}
+
+#copy all content from working tree to www new
+cp -r ${PATH_WT} ${PATH_WWW_NEW}
+
+#copy node_modules
+cp -r ${PATH_WWW}/node_modules ${PATH_WWW_NEW}
+
+#go to new and install packages
+cd ${PATH_WWW_NEW}
 npm prune
 npm install
-webpack
-if [ $? -ne 0 ]; then
-    cd ${BASE_PATH}/git/admin.$1.git
-    git --work-tree=${PATH_WT} reset --hard HEAD~
-    set +x
-    . ${PATH_WT}/.server/deploy/ascii/failed.sh "build failed!";
-fi
-mv ${PATH_HTML}/admin.$1 ${PATH_HTML}/admin.$1.old
-mv ${PATH_HTML}/admin.$1.new ${PATH_HTML}/admin.$1
-rm -fr ${PATH_HTML}/admin.$1.old
-pm2 reload admin.$1
+
+#make current to old
+mv ${PATH_WWW} ${PATH_WWW_OLD}
+
+#make new to current
+mv ${PATH_WWW_NEW} ${PATH_WWW}
+
+#remove old
+rm -fr ${PATH_WWW_OLD}
+
+#reload app
+pm2 reload ${PM2_APP_NAME}
 set +x
-. ${PATH_WT}/.server/deploy/ascii/success.sh "Complete $1 deploy successfully!"
+. ${PATH_WT}/.server/deploy/ascii/success.sh "Complete deploy successfully!"
