c28da2f70efdbc7057ec4863360a34f75c9529c8
Adidi <adielo@axonize.com>
2016-10-19 16:13:10 +0300

    locale en/he support

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,19 @@
 const express = require('express'),
     router = express.Router();
 
-router.get('/', (req, res, next) => {
-    res.render('index');
+router.get('/:locale?', (req, res, next) => {
+    let locale = req.params.locale,
+        clsDir;
+    if(locale === 'en'){
+        locale = require('../locale/en.json');
+        clsDir = 'ltr';
+    }
+    else{
+        locale = require('../locale/he.json');
+        clsDir = 'rtl'
+    }
+
+    res.render('index', {locale,clsDir});
 });
 
 module.exports = router;
